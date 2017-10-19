cdb213f71130c95ef59011776d019a61d881a0bf
bmodee <32397915+bmodee@users.noreply.github.com>
2017-10-19 17:40:31 +0200

    Add files via upload

--- laxdemo5.s
+++ laxdemo5.s
@@ -1,24 +1,23 @@
 MAIN		
 			LEA $8000,A7		; Sätter stackpekare
 			JSR SETUPPIA 		; initierar PIA
-			CLR.B D0			; Nollställ dataregister
-			(CLR.B D1 osv)
-			;;;;;;;;;;;
-
-			; vid avbrott:
-
-			MOVE.B #IRQA,$74 (/$68)
+			CLR.L D0			; Input
+			CLR.L D1			; Toggle 0 = norm 1 = inverterat
+			CLR.L D2
+			MOVE.L #STROBE,$74 	
 			AND.W #$F8FF,SR 	; sänker avbrottsnivån , måste finnas om avbrott är med
 
-			;;;;;;;;;;;
-
 LOOP		JMP LOOP
+
+STROBE		TST.B $10080
+
+
 
 SETUPPIA
 			CLR.B $10084  		; Välj DDRA
 			MOVE.B #0,$10080 	; alla pinnar är ingångar
-			MOVE.B #$04(/05 om avbrott används),$10084	; Välj PORTA
+			MOVE.B #$05,$10084	; Välj PORTA
 			CLR.B $10086  		; Välj DDRB
 			MOVE.B #$FF,$10082 	; alla pinnar är utgångar
-			MOVE.B #$04(/05 om avbrott används),$10086	; Välj PORTB
+			MOVE.B #$04,$10086	; Välj PORTB
 			RTS		
